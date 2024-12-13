deef5c9f71c876b7d9ba96c21002b79d29df53df
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-12-13 20:37:47 +0600

    Add error page component for meal loading failures

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to load meals, please try again later !</p>
+    </main>
+  );
+};
+
+export default ErrorPage;
