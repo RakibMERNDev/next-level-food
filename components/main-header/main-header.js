e1b28fe109bf82fd3c79339cc503f57cb05d8042
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-12-13 19:39:10 +0600

    Refactor MainHeader component to use NavLink for navigation and improve styling

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,8 +3,12 @@ import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import styles from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-bg";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
+
+
+
   return (
     <>
       <MainHeaderBackground />
@@ -16,10 +20,10 @@ const MainHeader = () => {
         <nav className={styles.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
