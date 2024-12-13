f648b8328375508aa23ef5a4859670e09a1c4d1c
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-12-13 12:54:42 +0600

    Refactor MainHeader component to improve image handling and code formatting

--- components/main-header.js
+++ components/main-header.js
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
-import styles from './main-header.module.css'
+import styles from "./main-header.module.css";
 
 const MainHeader = () => {
-
- 
   return (
     <header className={styles.header}>
       <Link href="/" className={styles.logo}>
-        <Image src={logoImg.src} width={100} height={100} alt="A plate with food on it" />
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevelFood
       </Link>
       <nav className={styles.nav}>
