7923110e1922aa5957a9c1130220ff2161fee61b
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-12-13 19:48:47 +0600

    Add Meals page layout and styling with meal item component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,26 @@
-
+import Link from "next/link";
+import styles from "./page.module.css";
 
 const MealsPage = () => {
-    return (
-        <div>
-            <h1>Meals Page</h1>
-        </div>
-    );
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>{" "}
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
