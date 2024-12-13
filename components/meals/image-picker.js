bddeaa0fd40df75528235893792ce50063212579
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-12-13 21:23:44 +0600

    Add ImagePicker component for meal sharing and integrate it into ShareMealPage

---
+++ components/meals/image-picker.js
@@ -0,0 +1,38 @@
+"use client";
+
+import { useRef } from "react";
+import styles from "./image-picker.module.css";
+
+const ImagePicker = ({ label, name }) => {
+
+const imageInput = useRef()
+
+  const handlePickClick = () => {
+    imageInput.current.click();
+  };
+
+  return (
+    <div className={styles.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={styles.controls}>
+        <input
+          className={styles.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+        />
+        <button
+          className={styles.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ImagePicker;
